bd2e93a1237e3930e039a9b0f1483b0e563d7a52
Johnston, Peter <peter.johnston@wizards.com>
2021-08-27 10:59:14 -0700

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -32,7 +32,6 @@ print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%
 # Module 3 code here:
 
 temperature_data = TemperatureData(data)
-<<<<<<< Updated upstream
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
@@ -41,12 +40,6 @@ recs = temperature_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
-=======
-recs = get_data_by_area(rec_area=test_area)
-print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
-print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
-
->>>>>>> Stashed changes
 
 # Module 4 code here:
 
